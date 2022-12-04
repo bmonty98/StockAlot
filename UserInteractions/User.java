0db80c6f9e9260c26a7626a10201f514be939fc9
bmonty98 <113128621+bmonty98@users.noreply.github.com>
2022-12-04 13:24:07 -0800

    most up to date version

--- UserInteractions/User.java
+++ UserInteractions/User.java
@@ -1,21 +1,22 @@
 package UserInteractions;
 
 import WebScrapper.*;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.HashMap;
+
+import org.omg.CORBA.portable.ValueBase;
 
 abstract public class User {
     private double capital = 0.0;
     private String name = "";
-    private LinkedList<stockObject> portfolio;
+    private HashMap<String, stockObject> portfolio;
     
     public User(){
         this.capital = 0.0;
         this.name = "John Doe";
-        this.portfolio = new LinkedList<stockObject>();
+        this.portfolio = new HashMap<String, stockObject>();
     }
 
-    public User(double capital, String name, LinkedList<stockObject> portfolio){
+    public User(double capital, String name, HashMap<String, stockObject> portfolio){
         this.capital = capital;
         this.name = name;
         this.portfolio = portfolio;
@@ -27,39 +28,74 @@ abstract public class User {
      */
     public stockObject getStock(String ticker){
         
-           ListIterator<stockObject> iterator = getPortfolio().ListIterator();
-           while (iterator.hasNext()){
-                stockObject current = iterator.next();
-                if(current.getTicker() == ticker){
-                    return current;
-                }
-           }
+        stockObject specifiedStock = this.portfolio.get(ticker);
+        if(specifiedStock == null){
+            throw new IllegalArgumentException("this ticker is either mispelled or not in your portfolio, try again.");
+        }
+        else{
+        return specifiedStock;
+        }   
     }
 
     /**
+     * for some reason wont let me make it a abstract public static method, so immitated the abstract ability.
      * @param ticker a String that represents the ticker of the stock.
      * @return NOTHING. just adds a specified stockObject to the object's portfolio variable.
      * then subtracts specified stocks price from User's capital variable.
      */
-   abstract public void buyStock(String ticker);
+     public void buyStock(String ticker){
+        // gets the desired stock for purchase:
+        try{
+            stockObject targetedStock = getHashed.getSFH(ticker);
+
+        // checks to see if User has the funds to purchase that stock and if so, does the transaction:
+            double targetedStockPrice = Double.parseDouble(targetedStock.getPrice());
+
+            if(this.capital - targetedStockPrice >= 0){
+                this.portfolio.put(ticker, targetedStock);
+                this.capital -= targetedStockPrice;
+            }
+        // happens if you do not have the funds to purchase the stock.
+            else{
+                throw new ArithmeticException("Invalid funds in your account to aquire this stock!");
+            }
+
+        }
+        // catching of the error
+        catch(ArithmeticException invalidFunds){
+            System.out.println(" You do not have the funds to be able to buy this stock, please aquire the dough to buy the stock before you waste my time.");
+            return;
+        }
+
+        return;
+     }
 
     /**
+     * for some reason wont let me make it a abstract public static method, so immitated the abstract ability.
      * @param ticker a String that represents the ticker of the stock.
      * @return NOTHING! Sells the specified stock, takes that stock out of players portfolio.
      *  adds its current price value of sold stock to Users capital variable.
      */
-    abstract public void sellStock(String ticker);
+    public void sellStock(String ticker){
+        try{
+
+            stockObject targetedStock = getHashed.getSFH(ticker);
+            
+            return;
+    }
 
     /**
     * if user runs out of money, and has no stocks to sell, then returns true.
     * if not the case, returns false.
      */
-     public Boolean bankruptcy(){
-        if(this.capital <= 0.0 && this.portfolio.size() == 0){
+    public Boolean bankruptcy(){
+        if(this.capital <= 0.0 && this.portfolio.isEmpty()){
             return true;
         }
         return false;
      }
+
+     // ------------------GETTER AND SETTTERS---------------
 
      public double getCapital(){
         return this.capital;
@@ -69,8 +105,23 @@ abstract public class User {
         return this.name;
      }
 
-     public LinkedList<stockObject> getPortfolio(){
+     public HashMap<String, stockObject> getPortfolio(){
         return this.portfolio;
+     }
+
+     public void setCapital(double capital){
+        this.capital = capital;
+        return;
+     }
+
+     public void setName(String name){
+        this.name = name;
+        return;
+     }
+
+     public void setPortfolio(HashMap<String, stockObject> portfolio){
+        this.portfolio = portfolio;
+        return;
      }
 
     /**
--- UserInteractions/UserInteractions.java
+++ UserInteractions/UserInteractions.java
@@ -1,19 +1,21 @@
-package UserInteractions;
-import WebScrapper.*;
 
-public class UserInteractions extends User {
+// IGNORE!!! THIS IS BROKEN AND HAS BEEN ABANDONED ATM.
+// package UserInteractions;
+// import WebScrapper.*;
 
-    @Override
-    public static void buyStock(String ticker){
-        // searches for the stock to see if its in the list of stocks to buy
-        stockObject targetedStock = getHashed.getSFH(ticker);
-        if(super.getCapital() >)
+// public class UserInteractions extends User {
+
+//     @Override
+//     public static void buyStock(String ticker){
+//         // searches for the stock to see if its in the list of stocks to buy
+//         stockObject targetedStock = getHashed.getSFH(ticker);
+//         if(super.getCapital() >)
 
 
 
-    }
+//     }
 
 
 
     
-}
+// }
