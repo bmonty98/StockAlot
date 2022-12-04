53406a65f1909b9cd9ff4913889edc7ce87ebeb0
Jason Tobin <94418504+JasonTobin@users.noreply.github.com>
2022-12-04 13:08:05 -0800

    Add files via upload

--- WebScrapper/getHashed.java
+++ WebScrapper/getHashed.java
@@ -2,6 +2,13 @@ package WebScrapper;
 
 public class getHashed {
     public static stockObject getSFH(String ticker) {
-        return WebScrapper.someHash.get(ticker);
+        if (WebScrapper.someHash.containsKey(ticker)) {
+            return WebScrapper.someHash.get(ticker);
+        }
+        else {
+            stockObject emptyStock = new stockObject();
+            System.out.println("Stock Ticker Not Found");
+            return emptyStock;
+        }
     }
 }
