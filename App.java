45c6deabffc354b6f797419b3cb95e688c46d49a
Jason Tobin <94418504+JasonTobin@users.noreply.github.com>
2022-12-06 18:28:59 -0800

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-import WebScrapper.*;
-import Draw.Frame;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        WebScrapper.readJSON();
-        Frame someFrame = new Frame(500, 1000);
-    }
-}
